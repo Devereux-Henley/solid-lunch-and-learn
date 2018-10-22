3e23ffdec15a5c787a8b95989b6661c84400accf
Henley_Contractor, Devereux <dhenley_Contractor@ntst.com>
2018-10-22 15:17:53 -0500

    Added fulcro system for auth to devcards.

--- src/cards/solid/intro.cljs
+++ src/cards/solid/intro.cljs
@@ -1,11 +1,32 @@
 (ns solid.intro
-  (:require [devcards.core :as rc :refer-macros [defcard]]
-            [solid.ui.components :as comp]))
+  (:require
+   [devcards.core :as rc :refer-macros [defcard]]
+   [solid.ui.components :as components]
+   [fulcro.client.cards :refer [defcard-fulcro]]
+   [fulcro.client.primitives :as prim :refer [defsc]]
+   [fulcro.client.dom :as dom]))
 
 (defcard LoginButton
-  "# Login Button"
-  (comp/ui-login-button {}))
+  "Creates a SOLID session for a user by presenting them with a login popup.
+
+   Visible when there is no current SOLID session."
+  (components/ui-login-button {}))
 
 (defcard LogoutButton
-  "# Logout Button"
-  (comp/ui-logout-button {:authentication/solid-session {}}))
+  "Removes a users SOLID session.
+
+   Visible when there is a current SOLID session."
+  (components/ui-logout-button {:authentication/solid-session {}}))
+
+(defsc AuthenticationRoot [this {:keys [login-button logout-button]}]
+  {:query [{:login-button (prim/get-query components/LoginButton)}
+           {:logout-button (prim/get-query components/LogoutButton)}]}
+  (dom/div nil
+    (components/ui-login-button login-button)
+    (components/ui-logout-button logout-button)))
+
+(defcard-fulcro AuthenticationButtons
+  "Demonstrates the authentication buttons interacting with each others state."
+  AuthenticationRoot
+  {}
+  {:inspect-data true})
