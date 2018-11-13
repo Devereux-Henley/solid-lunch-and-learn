aa379ad10dbfb8478d6998f6ee91906f64434763
Henley_Contractor, Devereux <dhenley_Contractor@ntst.com>
2018-11-13 10:31:32 -0600

    Added generic update operation.

--- src/main/solid/lib/core/operations.cljs
+++ src/main/solid/lib/core/operations.cljs
@@ -5,6 +5,10 @@
   [table state value id]
   (assoc-in state [table id] value))
 
+(defn update
+  [table state value id]
+  (update-in state [table id] merge value))
+
 (defn delete
   [table state id]
   (update-in state [table] dissoc id))
