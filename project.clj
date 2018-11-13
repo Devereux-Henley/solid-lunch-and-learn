e1c75a13293403a5e7d13622a4ccb237455ed7b4
Henley_Contractor, Devereux <dhenley_Contractor@ntst.com>
2018-11-13 16:38:53 -0600

    Version bump across the board, added promesa and cats.

--- project.clj
+++ project.clj
@@ -5,12 +5,14 @@
 
   :dependencies [[org.clojure/clojure     "1.9.0"]
                  [org.clojure/core.async  "0.4.474"]
-                 [thheller/shadow-cljs    "2.7.2"]
-                 [fulcrologic/fulcro      "2.6.14"]
+                 [thheller/shadow-cljs    "2.7.3"]
+                 [fulcrologic/fulcro      "2.6.15"]
+                 [funcool/cats            "2.3.1"]
+                 [funcool/promesa         "1.9.0"]
                  [com.wsscode/pathom      "2.2.0-beta14"]
 
                  ; only required if you want to use this for tests
-                 [fulcrologic/fulcro-spec "2.1.2" :scope "test" :exclusions [fulcrologic/fulcro]]]
+                 [fulcrologic/fulcro-spec "2.1.3" :scope "test" :exclusions [fulcrologic/fulcro]]]
 
   :source-paths ["src/main"]
   :test-paths ["src/test"]
@@ -21,8 +23,8 @@
                  :changes-only true}
 
   :profiles {:cljs       {:source-paths ["src/main" "src/test" "src/cards"]
-                          :dependencies [[binaryage/devtools "0.9.10"]
-                                         [org.clojure/core.async "0.4.474"]
+                          :dependencies [[binaryage/devtools         "0.9.10"]
+                                         [org.clojure/core.async     "0.4.474"]
                                          [fulcrologic/fulcro-inspect "2.2.4" :exclusions [fulcrologic/fulcro-css]]
-                                         [devcards "0.2.6" :exclusions [cljsjs/react cljsjs/react-dom]]]
+                                         [devcards                   "0.2.6" :exclusions [cljsjs/react cljsjs/react-dom]]]
                           }})
